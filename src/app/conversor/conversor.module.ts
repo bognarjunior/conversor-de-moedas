d8bb3e3cbbb9df8ce7a868cd4fdce0df88c45db5
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-23 16:17:57 -0300

    Implementando component
    Implementando o formulário de validação de moedas

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http'
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
@@ -9,7 +10,8 @@ import { MoedaService, ConversorService } from './services';
 @NgModule({
   imports: [
     CommonModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   declarations: [
     ConversorComponent
