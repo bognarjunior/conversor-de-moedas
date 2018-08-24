405f8c3141393ce164ab398b4961791587734682
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-24 13:14:42 -0300

    Criando Pipe
    Criando um filtro para transformar dada americana em formato brasileiro

--- src/app/conversor/components/conversor.component.ts
+++ src/app/conversor/components/conversor.component.ts
@@ -34,7 +34,7 @@ export class ConversorComponent implements OnInit {
    * @return void
    */
   init(): void {
-  	this.conversao = new Conversao('USD', 'BRL', null);
+  	this.conversao = new Conversao('EUR', 'BRL', null);
   	this.possuiErro = false;
   }
 
@@ -52,5 +52,5 @@ export class ConversorComponent implements OnInit {
         error => this.possuiErro = true
       );
   	}
-}
+  }
 }
